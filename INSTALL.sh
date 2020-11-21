a89d891863b7d5c395d25dbced75ce731bab3a39
Blaine Colwell <colwellblaine@gmail.com>
2020-11-21 17:29:55 -0500

    simplification

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,3 +4,5 @@ read INSTALL_LOCATION
 [ -z "$INSTALL_LOCATION" ] && INSTALL_LOCATION="/usr/local/bin"
 bash ./upda-inst/initialize-upda
 chmod -R 0755 *
+sudo cp scripts/* $INSTALL_LOCATION
+rm scripts/upda
